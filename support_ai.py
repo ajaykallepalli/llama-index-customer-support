e43b2e9ec54d3a3470175918da44434627b79d83
Ajay Kallepalli <ajay.r.kallepalli@gmail.com>
2024-09-07 18:51:57 -0700

    Making the storage of vector embeddings persistent.

--- support_ai.py
+++ support_ai.py
@@ -10,10 +10,20 @@ from llama_index.readers.json import JSONReader
 load_dotenv()
 Settings.llm = OpenAI(model="gpt-4o-mini", temperature=0.1)
 
+PERSIST_DIR = './storage'
 
-documents = SimpleDirectoryReader('data').load_data()
-#documents = reader.load_data(input_file='data/support_tickets.json', extra_info={})
-index = VectorStoreIndex.from_documents(documents, show_progress=True)
+
+if not os.path.exists(PERSIST_DIR):
+    #load the documents and create the index
+    documents = SimpleDirectoryReader('data').load_data(show_progress=True)
+    index = VectorStoreIndex.from_documents(documents, show_progress=True)
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    #load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
 query_engine = index.as_query_engine()
-response = query_engine.query("How do I reset my password?")
+
+response = query_engine.query("Did my laptop update, it says famalam?")
 print(response)
